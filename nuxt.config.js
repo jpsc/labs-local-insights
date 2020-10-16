2ab390bf5472d6e989a521206fd3fa038150aab7
jpsc <joao@funda.nl>
2020-10-16 22:28:28 +0200

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'local-insights',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  generate: {
+    cache: {
+      ignore: ['jest.config.js'], // ignore changes applied on this file
+    },
+  },
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    build: {
+      analyze: {
+        analyzerMode: 'static',
+      },
+    },
+    extractCSS: true,
+    extend(config, ctx) {},
+    html: {
+      minify: {
+        collapseBooleanAttributes: true,
+        decodeEntities: true,
+        minifyCSS: false,
+        minifyJS: false,
+        processConditionalComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        trimCustomFragments: true,
+        useShortDoctype: true,
+      },
+    },
+  },
+  vue: {
+    config: {
+      productionTip: false,
+    },
+  },
+  features: {
+    store: false,
+    // layouts: false,
+    transitions: false,
+    deprecations: false,
+    // middleware: false,
+    validate: false,
+    clientOnline: false,
+    clientPrefetch: false,
+  },
+}
